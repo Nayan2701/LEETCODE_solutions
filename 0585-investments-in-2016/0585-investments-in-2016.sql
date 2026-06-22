37d720400160a5af6f5ff803c5281c4b9cf679ed
Nayan Paliwal <90122933+Nayan2701@users.noreply.github.com>
2026-06-22 07:57:12 -0400

    Time: 354 ms (8.43%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,15 +1,18 @@
-# Write your MySQL query statement below
-SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
-FROM Insurance
-WHERE tiv_2015 IN(
-    SELECT tiv_2015
-    FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(*)>1
+-- Write your PostgreSQL query statement below
+-- Write your PostgreSQL query statement below
+WITH CheckedRows AS (
+    SELECT
+        tiv_2016,
+        COUNT(*) OVER (PARTITION BY tiv_2015) AS tiv_2015_count,
+        COUNT(*) OVER (PARTITION BY lat, lon) AS loc_count
+    FROM
+        Insurance
 )
-AND (lat,lon) IN(
-    SELECT lat,lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*)=1
-)
+
+SELECT
+    ROUND(SUM(tiv_2016)::numeric, 2) AS tiv_2016
+FROM
+    CheckedRows
+WHERE
+    tiv_2015_count > 1
+    AND loc_count = 1
