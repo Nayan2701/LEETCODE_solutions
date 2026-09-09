0a5ecf468c77e75c368ec26750b72f5531be234d
Nayan Paliwal <90122933+Nayan2701@users.noreply.github.com>
2026-09-09 19:48:36 -0400

    Time: 11 ms (77.31%), Space: 19.4 MB (75.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,12 @@
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
-            
-        # Array to store character counts for 'a' through 'z'
-        count = [0] * 26
-        
-        for i in range(len(s)):
-            # ord(char) - ord('a') maps 'a'->0, 'b'->1, ..., 'z'->25
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
-            
-        # If any element is not 0, the strings are not anagrams
-        for val in count:
-            if val != 0:
+        counter = {}
+        for char in s:
+            counter[char]=counter.get(char,0)+1
+        for char in t:
+            if char not in counter or counter[char]==0:
                 return False
-                
+            counter[char]-=1
         return True
